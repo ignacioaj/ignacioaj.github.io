afafa6a3e87840a9a5f0a6f6d82677c299dc8044
ignacioaj <ignacioatencia@gmail.com>
2025-12-07 23:36:34 +0100

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwind()],
-  },
+  integrations: [tailwind()], // <-- corregido aquí
+
   env: {
     schema: {
       RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
       MY_EMAIL: envField.string({ context: "server", access: "secret" }),
     },
   },
+
   site: "https://ignacioaj.github.io",
   output: "static",
 });
